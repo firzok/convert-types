e9e8dcc07167257a257fe920839934c62e1e2caa
Firzok Nadeem <firzok.nadeem@gmail.com>
2020-06-03 01:30:23 +0500

    added two dropdowns for data types

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const programmingLanguages = [
+  "C",
+  "C++",
+  "C#",
+  "Java",
+  "JavaScript",
+  "Objective-C",
+  "PHP",
+  "Python",
+  "Ruby",
+  "Swift"
+];
+
+export { programmingLanguages };
